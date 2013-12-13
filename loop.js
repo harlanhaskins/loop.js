a775f776d2db2398241ebc6f9efaa87a0dfcdcca
Harlan <harlan@harlanhaskins.com>
2013-12-13 15:47:13 -0500

    Removed GPL frop top of file.

--- loop.js
+++ loop.js
@@ -1,22 +1,7 @@
 /*
-Written by Harlan Haskins.
-Copyright 2013, Not So Average, Inc.
-	
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+Copyright 2013 Harlan Haskins
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-	You must have a folder filled with your images, named sequentially with a 3 digit number, starting with 000.
-	
+You must have a folder filled with your images, named sequentially with a 3 digit number, starting with 000.
 */
 
 /*jshint browser:true */
